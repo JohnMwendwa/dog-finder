1c61cb95ea5d2d1663a51d2dd7bb8e28576f8c1b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-13 13:24:35 +0300

    create a global navbar to be displayed in every page/route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar />
     </div>
   );
 }
 
 export default App;
+ 
