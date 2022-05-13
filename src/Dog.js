90d795b4c8d6abebb2603331c2a98412dd706c00
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-13 17:57:12 +0300

    display dog images and name in index route

--- src/Dog.js
+++ src/Dog.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
-function Dog() {
-  return (
-    <div>Dog</div>
-  )
+class Dog extends Component {
+  render() {
+      let {name,src} =this.props
+    return (
+      <div className='Dog'>  
+        <Link to={`/dogs/${name}`}>
+          <img src={src} alt={name} />
+            {name}
+         </Link>
+      </div>
+    )
+  }
 }
 
 export default Dog
