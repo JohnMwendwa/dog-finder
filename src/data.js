ecd1fd754e0e0c52ac8b0c7b1dd775bcdbb9f82d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-13 13:32:08 +0300

    create the dogs data file

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const dogs = [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: "whiskey",
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Hazel",
+      age: 3,
+      src: "hazel",
+      facts: [
+        "Hazel has soooo much energy!",
+        "Hazel is highly intelligent.",
+        "Hazel loves people more than dogs."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: "tubby",
+      facts: [
+        "Tubby is not the brightest dog",
+        "Tubby does not like walks or exercise.",
+        "Tubby loves eating food."
+      ]
+    }
+  ]
+
+export default dogs;
