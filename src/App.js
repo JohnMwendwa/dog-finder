28e6dd24551fdf2ea1b8533c1618fe905e777c5e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-13 18:01:27 +0300

    add the default app routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Routes,Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
+import Dog from './Dog';
+import Home from './Home';
+import DogDetails from './DogDetails';
+import {getDogs} from './data';
 
 
 function App() {
+  let dogs = getDogs();
   return (
-    <div className="App">
+    <div className="App">  
      <Navbar />
+
+     <Routes>
+       <Route index element={<Home dogs={dogs}/>} />
+       <Route path='dogs' element={<Dog />} />
+       <Route path="dogs/:name"  element={<DogDetails />} />
+     </Routes>
     </div>
   );
 }
