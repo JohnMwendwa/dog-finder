8b1069e49bd1ba9511e4f0b1bff5f863a6ae6143
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-13 18:24:01 +0300

    display the individual dog details,the image,name,age and facts

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import {useNavigate,useParams} from 'react-router-dom'
+import {getDogs,searchDog} from './data'
 
 function DogDetails() {
+  let params = useParams();
+  let navigate =useNavigate();
+  let dogs = getDogs();
+  let dog = searchDog(dogs,params.name)
   return (
-    <div>DogDetails</div>
+    <div className='DogDetails'>
+      <img src={dog.src} alt={dog.name} />
+      <h2>{dog.name}</h2>
+      <h4>Age: {dog.age}</h4>
+      <ul>
+        {dog.facts.map((fact,idx)=>(
+          <li key={idx}>{fact}</li>
+        ))}
+      </ul>
+      <button onClick={()=>navigate(-1)}>Go Back</button>
+    </div>
   )
 }
 
