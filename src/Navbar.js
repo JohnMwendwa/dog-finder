e2ba841cb330aef67ccb9cb54f5204ee59ffbe01
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-13 14:36:36 +0300

    import the new method from datafile

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import dogs from './data'
+import {getDogs} from './data'
 
 function Navbar() {   
+  let dogs = getDogs();
   return (
     <div className='Navbar'>
         {dogs.map(dog=>(
